a2825c546232d22be20797241d7acb13ee976c42
unknown <sudip.@neupane.com>
2023-07-28 10:02:54 +0545

    blog models

---
+++ blog/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class BlogPost(models.Model):
+    title = model.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+
+    def __str__(self):
+        return self.title
+    
+
+class Comment(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    post  = models.ForeignKey(BlogPost, on_delete=models.CASCADE, related_name= 'comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return f"Comment by{self.author.username} on {self.post.title}"
